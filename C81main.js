597fefce018c32e000f1f23b9331145295cc6c0e
Savya-Sarda <86636104+Savya-Sarda@users.noreply.github.com>
2021-09-22 11:47:31 +0800

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -9,28 +9,25 @@ ctx.arc(250,210,40,0,2 * Math.PI)
 ctx.stroke();
 
 ctx.beginPath();
-ctx.strokeStyle="red"
+ctx.strokeStyle="yellow"
 ctx.lineWidth=5;
-ctx.arc(250,210,40,0,2 * Math.PI)
+ctx.arc(300,230,40,0,2 * Math.PI)
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle="black" 
 ctx.lineWidth=5;
-ctx.arc(250,210,40,0,2 * Math.PI)
+ctx.arc(350,210,40,0,2 * Math.PI)
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle="green"
 ctx.lineWidth=5;
-ctx.arc(250,210,40,0,2 * Math.PI)
+ctx.arc(400,230,40,0,2 * Math.PI)
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle="red"
 ctx.lineWidth=5;
-ctx.arc(250,210,40,0,2 * Math.PI)
-ctx.stroke();
- 
-}
- 
+ctx.arc(450,210,40,0,2 * Math.PI)
+ctx.stroke(); 
